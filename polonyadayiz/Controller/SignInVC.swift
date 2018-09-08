726209dca62a0e7774e4ffa2c358c2c3cda792f9
emir_kartal <kartal.emirr@gmail.com>
2018-09-08 23:41:11 +0200

    add login screen

---
+++ polonyadayiz/Controller/SignInVC.swift
@@ -0,0 +1,34 @@
+//
+//  SignInVC.swift
+//  polonyadayiz
+//
+//  Created by Emir Kartal on 8.09.2018.
+//  Copyright © 2018 Emir Kartal. All rights reserved.
+//
+
+import UIKit
+
+class SignInVC: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func signInBtnTapped(_ sender: UIButton) {
+    }
+    @IBAction func signUpBtnTapped(_ sender: UIButton) {
+    }
+    
+   
+}
