957386b9d4bbd0ffe20b50cafe6cf9103c58bff7
emir_kartal <kartal.emirr@gmail.com>
2018-09-13 20:16:52 +0200

    Show favourites in myfavourites section

--- polonyadayiz/Controller/MyFavouritesVC.swift
+++ polonyadayiz/Controller/MyFavouritesVC.swift
@@ -11,6 +11,8 @@ import CoreData
 
 class MyFavouritesVC: UIViewController, NavBarImageProtocol {
     
+    @IBOutlet weak var favouriteTableView: UITableView!
+    
     var favouriteDepartmentArray : [LikedDepartment] = []
     let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
@@ -18,20 +20,45 @@ class MyFavouritesVC: UIViewController, NavBarImageProtocol {
         super.viewDidLoad()
 
         addNavBarImage()
+        
+        favouriteTableView.delegate = self
+        favouriteTableView.dataSource = self
+        
         loadFavouriteData()
         
-        
     }
+    
+    //MARK: Load data from Coredata
     
     func loadFavouriteData() {
         
         let request : NSFetchRequest<LikedDepartment> = LikedDepartment.fetchRequest()
         do {
             favouriteDepartmentArray = try context.fetch(request)
+            favouriteTableView.reloadData()
         } catch {
             print("error \(error.localizedDescription)")
         }
         
     }
 
+}
+
+//MARK: Tableview delegate and data source methods
+
+extension MyFavouritesVC : UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favouriteDepartmentArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "favouriteCell", for: indexPath)
+        
+        cell.textLabel?.text = favouriteDepartmentArray[indexPath.row].departmentName
+        cell.detailTextLabel?.text = favouriteDepartmentArray[indexPath.row].universityName
+        
+        return cell
+    }
+    
+    
 }
